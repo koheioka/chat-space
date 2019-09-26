46c234f1b66cb1e68dae401008ed482d2b63c6a9
koheioka <kouhei0205.1992@gmail.com>
2019-09-26 16:15:55 +0900

    message_utomatic_updating_edit

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,8 @@
 $(function(){
   function buildHTML(message){
     var content = message.content ? `${ message.content }` : "";
-    var image = message.image ? `<img src= ${ message.image }>` : "";
-    var html = `<div class="feature__body--right-center-comment">
+    var image = message.image ? `<img class="lower-info__image" src="${ message.image }">` : "";
+    var html = `<div class="feature__body--right-center-comment" data-message-id=${message.id}>
                   <div class="feature__body--right-center-comment-box">
                     <div class="feature__body--right-center-comment-box-member">
                       ${message.user_name}
@@ -21,7 +21,6 @@ $(function(){
     return html;
   }
 
-
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -36,7 +35,7 @@ $(function(){
     })
     .done(function(data){
       var html = buildHTML(data);
-      $('.feature__body--right-center').append(html)
+      $('.feature__body--right-center').append(html);
       $("#new_message")[0].reset();
       $('.feature__body--right-center').animate({ scrollTop: $('.feature__body--right-center')[0].scrollHeight});
       return false
@@ -48,4 +47,28 @@ $(function(){
       $('.feature__body--right-bottom-box-text-send').prop('disabled', false);
     })
   })
+
+  var reloadMessages = function() {
+    var group_id = $('.top__head--right-group-name').data('group-id');
+    var last_message_id = $('.feature__body--right-center-comment:last').data('message-id'); 
+    console.log(last_message_id)
+    $.ajax({ 
+      url: `/groups/${group_id}/api/messages`, 
+      type: 'get', 
+      dataType: 'json', 
+      data: {id: last_message_id} 
+    })
+    .done(function (messages){
+      var insertHTML = '';
+      messages.forEach(function(message){
+        insertHTML = buildHTML(message)
+        $('.feature__body--right-center').append(insertHTML);
+        $('.feature__body--right-center').animate({ scrollTop: $('.feature__body--right-center')[0].scrollHeight});
+      })
+    })
+    .fail(function () {
+      console.log('error');
+    });
+  }
+  setInterval(reloadMessages, 5000);
 });
